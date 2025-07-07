506d2198f5762b9a87447e96174d9c65fbd01177
lc1006 <1176168734@qq.com>
2025-07-07 12:18:21 +0800

    feat:update upm

--- Runtime/Public/TapTapAchievement.cs
+++ Runtime/Public/TapTapAchievement.cs
@@ -5,7 +5,7 @@ namespace TapSDK.Achievement
 {
     public class TapTapAchievement
     {
-        public static readonly string Version = "4.6.3";
+        public static readonly string Version = "4.7.0-alpha.14";
 
         public static void Increment(string achievementId, int step)
         {
